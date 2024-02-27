a228ea8cd1f668eabb4384ab7824a58d9cbb3d73
KirillKurril <kirillkurill1525@gmail.com>
2024-02-27 23:26:02 +0300

    gui init

---
+++ BackgammonLib/UserInterface/Backgammon.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UserInterface
+{
+    /// <summary>
+    /// Логика взаимодействия для Backgammon.xaml
+    /// </summary>
+    public partial class Backgammon : Page
+    {
+        public Backgammon()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
